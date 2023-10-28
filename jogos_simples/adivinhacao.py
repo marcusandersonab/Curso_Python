50f26c92e7a634288734efbad333ca4d2fb6128a
Marcus <marcus@dee.ufc.br>
2023-10-28 10:55:12 -0300

    nada

--- jogos_simples/adivinhacao.py
+++ jogos_simples/adivinhacao.py
@@ -1,12 +1,11 @@
 import random
-
 def jogar():
     print("*********************************")
     print("Bem vindo ao jogo de Adivinhação!")
     print("*********************************")
 
     # numero_secreto = round(random.random()*100) #0.0 0.1
-    numero_secreto = round(random.randrange(1,101)) #1 100 1
+    numero_secreto = round(random.randrange(1, 101))  # 1 100 1
     total_de_tentativas = 0
     pontos = 1000
 
@@ -15,9 +14,9 @@ def jogar():
 
     nivel = int(input("Defina o nível:"))
 
-    if (nivel == 1):
+    if nivel == 1:
         total_de_tentativas = 20
-    elif (nivel == 2):
+    elif nivel == 2:
         total_de_tentativas = 10
     else:
         total_de_tentativas = 5
@@ -29,25 +28,26 @@ def jogar():
         print("Você digitou ", chute_str)
         chute = int(chute_str)
 
-        if (chute < 1 or chute > 100):
+        if chute < 1 or chute > 100:
             print("Você deve digitar um número entre 1 e 100!")
             continue
         acertou = chute == numero_secreto
-        maior   = chute > numero_secreto
-        menor   = chute < numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
 
-        if (acertou):
+        if acertou:
             print("Você acertou e fez {} pontos!".format(pontos))
             break
         else:
-            if (maior):
+            if maior:
                 print("Você errou! Seu chute foi maior que o número secreto.")
-            elif (menor):
+            elif menor:
                 print("Você errou! Seu chute foi menor que o número secreto.")
             pontos_perdidos = abs(numero_secreto - chute)
             pontos -= pontos_perdidos
         # rodada += 1
     print("Fim do jogo")
 
-if (__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
